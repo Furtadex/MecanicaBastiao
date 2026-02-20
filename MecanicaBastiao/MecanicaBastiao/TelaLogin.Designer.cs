e7c52642a352ab1893a02a733527c7bcfba09530
GUILHERME ALEXANDRE FURTADO HERNANDEZ <05093348066@senacrs.edu.br>
2026-02-19 22:03:12 -0300

    Atualizado 19/02/26 com login pré pronto.

--- MecanicaBastiao/MecanicaBastiao/TelaLogin.Designer.cs
+++ MecanicaBastiao/MecanicaBastiao/TelaLogin.Designer.cs
@@ -30,40 +30,187 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaLogin));
             panel1 = new Panel();
+            linkLabel1 = new LinkLabel();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            pictureBox3 = new PictureBox();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
             button1 = new Button();
+            pictureBox4 = new PictureBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = SystemColors.ButtonHighlight;
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(button1);
             panel1.Location = new Point(387, 120);
             panel1.Name = "panel1";
-            panel1.Size = new Size(426, 543);
+            panel1.Size = new Size(447, 607);
             panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.FromArgb(107, 33, 168);
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkArea = new LinkArea(0, 0);
+            linkLabel1.LinkColor = Color.FromArgb(255, 128, 0);
+            linkLabel1.LinkVisited = true;
+            linkLabel1.Location = new Point(309, 398);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(105, 15);
+            linkLabel1.TabIndex = 11;
+            linkLabel1.Text = "Esqueceu a senha?";
+            linkLabel1.VisitedLinkColor = Color.FromArgb(255, 128, 0);
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(107, 33, 168);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(32, 441);
+            button3.Name = "button3";
+            button3.Size = new Size(383, 50);
+            button3.TabIndex = 10;
+            button3.Text = "Entrar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(32, 328);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(383, 50);
+            textBox2.TabIndex = 8;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(185, 300);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(75, 22);
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 14F);
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(32, 227);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(383, 50);
+            textBox1.TabIndex = 6;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(172, 194);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(93, 27);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.ForeColor = SystemColors.GrayText;
+            label3.Location = new Point(85, 155);
+            label3.Name = "label3";
+            label3.Size = new Size(277, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Acesso o painel de gestão da Locadora";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(96, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(255, 32);
+            label2.TabIndex = 3;
+            label2.Text = "Login Administrativo";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12.5F, FontStyle.Bold);
+            label1.Location = new Point(309, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Administrador";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(256, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(47, 48);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button1.ForeColor = Color.Black;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(17, 18);
+            button1.Location = new Point(26, 30);
             button1.Name = "button1";
             button1.Size = new Size(100, 37);
             button1.TabIndex = 0;
             button1.Text = "     Voltar";
             button1.UseVisualStyleBackColor = true;
             // 
-            // Login
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(17, 509);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(413, 62);
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            // 
+            // TelaLogin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1184, 761);
             Controls.Add(panel1);
-            Name = "Login";
+            Name = "TelaLogin";
             Text = "Login";
             panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
@@ -71,5 +218,16 @@
 
         private Panel panel1;
         private Button button1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox2;
+        private TextBox textBox1;
+        private PictureBox pictureBox3;
+        private TextBox textBox2;
+        private Button button3;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox4;
     }
 }
